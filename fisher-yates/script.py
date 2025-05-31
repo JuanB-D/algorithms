f0b762b9bf0a187d4d2e0f12e48a4aeef2b11ddc
felipe <felipecallebd@gmail.com>
2025-05-31 15:44:34 -0500

    changed py

--- fisher-yates/script.py
+++ fisher-yates/script.py
@@ -1,15 +1,18 @@
 import random
 
 lista = [1, 2, 3, 4, 5]
-
+history = []
 def fisher_yates_shuffle(array):
     for i in range(len(array) -1, 0, -1):
         j = random.randint(0, i)
         array[i], array[j] = array[j], array[i]
+        step = array[:]
+        history.append(step)
     return array
 
 
 print(fisher_yates_shuffle(lista))
+print(history)
 
 # complejidad O(n)
 
